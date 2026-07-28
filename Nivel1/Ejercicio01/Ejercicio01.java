14532263b06e8ec94141b0490476e7c2ad756578
Adrianna Agramonte <aagramontel@miumg.edu.gt>
2026-07-28 11:03:40 -0600

    Ejercicio 1 - Nivel 1 Creado

---
+++ Nivel1/Ejercicio01/Ejercicio01.java
@@ -0,0 +1,16 @@
+package Nivel1.Ejercicio01;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 0;
+
+            int resultado = a / b;
+
+            System.out.println("Resultado: " + resultado);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: No se puede dividir entre cero.");
+        }
+    }
+}
